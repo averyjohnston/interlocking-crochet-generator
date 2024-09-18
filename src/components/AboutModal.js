c125885a370f91f224097f71e600c6dea846d350
Avery Johnston <90629384+averyjohnston@users.noreply.github.com>
2024-09-18 15:40:26 -0500

    Update AboutModal.js

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -5,15 +5,13 @@ export default function AboutModal(props) {
 	return (
 		<Modal handleCloseClick={props.handleCloseClick}>
 			<h1>About ICG</h1>
-			<p>Interlocking Crochet Generator, by Amanda</p>
+			<p>Interlocking Crochet Generator</p>
 			<p>Patterns are written assuming you know the basics of interlocking crochet, also called mosaic crochet, interlocking filet crochet, and intermeshing crochet.</p>
 			<p>
 				The starting row for the foreground color uses a long chain method, instead of two separate meshes, for maximum flexibility for the bottom rows.
 				If you like, you can start with two meshes instead, although the finished product might not look exactly the same as the grid you draw.
 			</p>
 			<p><strong>Please don't sell patterns created in this tool.</strong> You can sell the finished product though. Credit is highly appreciated!</p>
-			<p>Find a bug? Email me at <a href="mailto:crochetgeneratorcontact@gmail.com">crochetgeneratorcontact@gmail.com</a></p>
-			<p className="Modal__footer">Made in ReactJS. <a target="_blank" rel="noopener noreferrer" href="https://github.com/amandaejohnston/interlocking-crochet-generator">Source code</a></p>
 		</Modal>
 	);
 }
